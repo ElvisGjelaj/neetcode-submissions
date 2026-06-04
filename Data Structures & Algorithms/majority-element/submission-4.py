de42a8e02dcc99079d48ab7210d4a2353eb2778b
Elvis Gjelaj <gjelajelvis1@gmail.com>
2026-06-04 09:29:58 -0400

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        champ_num = -1
+        champ_occurances = 0
+        elem_dict = {}
+        for num in nums:
+            try:
+                elem_dict[num] += 1
+            except:
+                elem_dict[num] = 1
+            if elem_dict[num] > champ_occurances:
+                champ_num = num
+                champ_occurances = elem_dict[num]
+        return champ_num
